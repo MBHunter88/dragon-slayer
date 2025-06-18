170b6a5b1654f3422b744c098a5e07321005969f
MBHunter88 <130544940+MBHunter88@users.noreply.github.com>
2025-06-17 22:00:02 -0700

    Fix StartMenu component definition and render in App

--- src/components/StartMenu.jsx
+++ src/components/StartMenu.jsx
@@ -2,24 +2,24 @@ import React from "react";
 import "./App/App.css";
 import dragonStartImg from "./App/assests/dragonbackground.png";
 
-
-    return (
-        <>
-        <div className="game-container">
+const StartMenu = ({ onStart }) => {
+  return (
+    <>
+      <div className="game-container">
         <h1>Dragon Slayer</h1>
         <img src={dragonStartImg} alt="Dragon with a fiery background" />
-        <p>The fate of the kingdom rests in your hands. 
-          Begin your journey and prepare to face the challenges ahead. 
-          Are you ready to step into the world of adventure, fight 
-          fearsome creatures, and protect the realm?</p>
+        <p>
+          The fate of the kingdom rests in your hands. Begin your journey and
+          prepare to face the challenges ahead. Are you ready to step into the
+          world of adventure, fight fearsome creatures, and protect the realm?
+        </p>
         <div className="button-container">
-        <button onClick={onStart}>Start Game</button>
+          <button onClick={onStart}>Start Game</button>
+        </div>
+      </div>
+    </>
+  );
+};
 
-      </div>
-      </div>
-      </>
-    );
-  };
-  
-  export default StartMenu;
+export default StartMenu;
 
